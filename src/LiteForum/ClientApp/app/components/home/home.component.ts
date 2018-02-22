1ef001c3b90d8b190d31e30fb965cad0261ad7c8
codeprefect <mubarakadeimam@gmail.com>
2018-02-22 22:02:43 +0100

    created content card for post

--- src/LiteForum/ClientApp/app/components/home/home.component.ts
+++ src/LiteForum/ClientApp/app/components/home/home.component.ts
@@ -5,7 +5,8 @@ import { AlertService } from '../../_services/alert.service';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
     posts: Post[] = [];
