d147e5766d37a6e03d2d058e9f0299839b9bc3be
codeprefect <mubarakadeimam@gmail.com>
2018-02-22 15:25:00 +0100

    added migrations for category addition

---
+++ database/LF002__added_categories.sql
@@ -0,0 +1,35 @@
+﻿EXEC sp_rename N'Posts.Content', N'Title', N'COLUMN';
+
+GO
+
+ALTER TABLE [Posts] ADD [CategoryId] int NOT NULL DEFAULT 0;
+
+GO
+
+CREATE TABLE [Category] (
+    [Id] int NOT NULL IDENTITY,
+    [CreatedBy] nvarchar(max) NULL,
+    [CreatedDate] datetime2 NOT NULL,
+    [Deleted] datetime2 NULL,
+    [ModifiedBy] nvarchar(max) NULL,
+    [ModifiedDate] datetime2 NULL,
+    [Name] nvarchar(max) NULL,
+    [Version] rowversion NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE INDEX [IX_Posts_CategoryId] ON [Posts] ([CategoryId]);
+
+GO
+
+ALTER TABLE [Posts] ADD CONSTRAINT [FK_Posts_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE CASCADE;
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20180222141649_added category table', N'2.0.1-rtm-125');
+
+GO
+
--- src/LiteForum/Migrations/20180222141649_added category table.Designer.cs
+++ src/LiteForum/Migrations/20180222141649_added category table.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace LiteForum.Migrations
 {
     [DbContext(typeof(LiteForumDbContext))]
-    partial class LiteForumDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180222141649_added category table")]
+    partial class addedcategorytable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
